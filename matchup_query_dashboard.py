4d41d245935e4334fc56ff66c4e983456788ca8c
neptune0518 <brandonmcohen@gmail.com>
2025-03-18 22:41:46 -0400

    Update matchup_query_dashboard.py

--- matchup_query_dashboard.py
+++ matchup_query_dashboard.py
@@ -7,7 +7,8 @@ from io import StringIO
 file_paths = {
     "Defense": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/defense25.csv",
     "Height": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/height25.csv",
-    "Misc": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/misc25.csv",
+    "Misc Offense": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/misc25Offense.csv",
+    "Misc Defense": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/misc25Defense.csv",
     "Offense": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/offense25.csv",
     "Point Distribution": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/pointdist25.csv",
     "Summary": "https://raw.githubusercontent.com/neptune0518/matchup-analyzer/main/summary25.csv"
